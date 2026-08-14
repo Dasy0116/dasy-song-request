34562a66cfcca4a8e1441537649e223c4c6a1545
Dasy0116 <2021967816@qq.com>
2026-08-14 12:17:18 +0800

    fix: 修复GitHub Pages白屏 - 切换HashRouter+添加SPA回退

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 import Home from "@/pages/Home";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Home />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
